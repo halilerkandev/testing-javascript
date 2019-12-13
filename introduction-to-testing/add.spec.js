ca2272431c4b50c607dfbd797b455456bd0c150c
Halil Erkan Dursunoğlu <h.erkan.dursunoglu@gmail.com>
2019-12-14 00:02:23 +0300

    core assert module added

--- introduction-to-testing/add.spec.js
+++ introduction-to-testing/add.spec.js
@@ -1,9 +1,15 @@
+const assert = require('assert');
 const add = require('./add');
 
 const result = add(1, 3);
 
-if (result == 4) {
-  console.log('tests pass.');
-} else {
-  throw new Error('Expected 1 + 3 to equal 4.');
-}
+const expected = 4;
+
+// basic assertion
+// if (result == expected) {
+//   console.log('tests pass.');
+// } else {
+//   throw new Error('Expected 1 + 3 to equal 4.');
+// }
+
+assert.equal(result, expected);
