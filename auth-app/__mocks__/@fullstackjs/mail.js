c901a995ba34fe8d819fb4818d173b88362783d8
Halil Erkan Dursunoğlu <h.erkan.dursunoglu@gmail.com>
2019-12-15 23:46:16 +0300

    added jest config and set connection in beforeAll/afterAll

---
+++ auth-app/__mocks__/@fullstackjs/mail.js
@@ -0,0 +1,14 @@
+module.exports = class Mail {
+  to() {
+    return this;
+  }
+  subject() {
+    return this;
+  }
+  data() {
+    return this;
+  }
+  send() {
+    return this;
+  }
+};
--- auth-app/__tests__/server/models/user.spec.js
+++ auth-app/__tests__/server/models/user.spec.js
@@ -3,11 +3,17 @@ import mongoose from 'mongoose';
 import Bcrypt from 'bcryptjs';
 
 describe('The user model', () => {
-  it('should hash the user password before saving to the database', async () => {
+  beforeAll(async () => {
     await mongoose.connect(
       'mongodb://erkan:erkan1234@ds255728.mlab.com:55728/auth-app',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
     );
+  });
 
+  it('should hash the user password before saving to the database', async () => {
     const user = {
       name: 'Test User',
       email: 'test@user.com',
@@ -17,5 +23,21 @@ describe('The user model', () => {
     const createdUser = await User.create(user);
 
     expect(Bcrypt.compareSync(user.password, createdUser.password)).toBe(true);
+  });
+
+  it('should set the email confirm code for the user before saving to the database', async () => {
+    const user = {
+      name: 'Test User',
+      email: 'test@user.com',
+      password: 'password',
+    };
+
+    const createdUser = await User.create(user);
+
+    expect(createdUser.emailConfirmCode).toEqual(expect.any(String));
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
   });
 });
