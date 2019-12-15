ef4bbd4e0f7cf5acecc9c7058363900599678e48
Halil Erkan Dursunoğlu <h.erkan.dursunoglu@gmail.com>
2019-12-16 00:14:54 +0300

    testing instance methods

--- auth-app/__tests__/server/models/user.spec.js
+++ auth-app/__tests__/server/models/user.spec.js
@@ -1,8 +1,18 @@
 import User from '@models/User';
 import mongoose from 'mongoose';
 import Bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from '@config';
 
 describe('The user model', () => {
+  const user = {
+    name: 'Test User',
+    email: 'test@user.com',
+    password: 'password',
+  };
+
+  let createdUser;
+
   beforeAll(async () => {
     await mongoose.connect(
       'mongodb://erkan:erkan1234@ds255728.mlab.com:55728/auth-app',
@@ -11,30 +21,25 @@ describe('The user model', () => {
         useUnifiedTopology: true,
       },
     );
+    createdUser = await User.create(user);
   });
 
   it('should hash the user password before saving to the database', async () => {
-    const user = {
-      name: 'Test User',
-      email: 'test@user.com',
-      password: 'password',
-    };
-
-    const createdUser = await User.create(user);
-
     expect(Bcrypt.compareSync(user.password, createdUser.password)).toBe(true);
   });
 
   it('should set the email confirm code for the user before saving to the database', async () => {
-    const user = {
-      name: 'Test User',
-      email: 'test@user.com',
-      password: 'password',
-    };
-
-    const createdUser = await User.create(user);
-
     expect(createdUser.emailConfirmCode).toEqual(expect.any(String));
+  });
+
+  describe('The generateToken method', () => {
+    it('should generate a valid jwt for a user', () => {
+      const token = createdUser.generateToken();
+
+      const { id } = jwt.verify(token, config.jwtSecret);
+
+      expect(id).toEqual(JSON.parse(JSON.stringify(createdUser._id)));
+    });
   });
 
   afterAll(async () => {
